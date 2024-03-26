3cd62f5a67cb664d4dd93f8e863480a1e74e5b78
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2024-03-26 14:03:26 +0300

    Add files via upload

---
+++ vt_ornek/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace vt_ornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvFilmler = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFilmAd = new System.Windows.Forms.TextBox();
+            this.txtYonetmen = new System.Windows.Forms.TextBox();
+            this.txtYil = new System.Windows.Forms.TextBox();
+            this.txtTür = new System.Windows.Forms.TextBox();
+            this.txtSüre = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvFilmler
+            // 
+            this.dgvFilmler.AllowUserToAddRows = false;
+            this.dgvFilmler.AllowUserToDeleteRows = false;
+            this.dgvFilmler.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvFilmler.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvFilmler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFilmler.Location = new System.Drawing.Point(12, 258);
+            this.dgvFilmler.Name = "dgvFilmler";
+            this.dgvFilmler.ReadOnly = true;
+            this.dgvFilmler.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFilmler.Size = new System.Drawing.Size(776, 180);
+            this.dgvFilmler.TabIndex = 0;
+            this.dgvFilmler.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFilmler_CellEnter);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(33, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Yönetmen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(95, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Yıl";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(89, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tür";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(55, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Film Ad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(77, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Süre";
+            // 
+            // txtFilmAd
+            // 
+            this.txtFilmAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFilmAd.Location = new System.Drawing.Point(130, 47);
+            this.txtFilmAd.Multiline = true;
+            this.txtFilmAd.Name = "txtFilmAd";
+            this.txtFilmAd.Size = new System.Drawing.Size(161, 28);
+            this.txtFilmAd.TabIndex = 7;
+            // 
+            // txtYonetmen
+            // 
+            this.txtYonetmen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYonetmen.Location = new System.Drawing.Point(130, 81);
+            this.txtYonetmen.Multiline = true;
+            this.txtYonetmen.Name = "txtYonetmen";
+            this.txtYonetmen.Size = new System.Drawing.Size(161, 28);
+            this.txtYonetmen.TabIndex = 8;
+            // 
+            // txtYil
+            // 
+            this.txtYil.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtYil.Location = new System.Drawing.Point(130, 115);
+            this.txtYil.Multiline = true;
+            this.txtYil.Name = "txtYil";
+            this.txtYil.Size = new System.Drawing.Size(161, 28);
+            this.txtYil.TabIndex = 9;
+            // 
+            // txtTür
+            // 
+            this.txtTür.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTür.Location = new System.Drawing.Point(130, 149);
+            this.txtTür.Multiline = true;
+            this.txtTür.Name = "txtTür";
+            this.txtTür.Size = new System.Drawing.Size(161, 28);
+            this.txtTür.TabIndex = 10;
+            // 
+            // txtSüre
+            // 
+            this.txtSüre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSüre.Location = new System.Drawing.Point(130, 183);
+            this.txtSüre.Multiline = true;
+            this.txtSüre.Name = "txtSüre";
+            this.txtSüre.Size = new System.Drawing.Size(161, 28);
+            this.txtSüre.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSüre);
+            this.Controls.Add(this.txtTür);
+            this.Controls.Add(this.txtYil);
+            this.Controls.Add(this.txtYonetmen);
+            this.Controls.Add(this.txtFilmAd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvFilmler);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFilmler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFilmler;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFilmAd;
+        private System.Windows.Forms.TextBox txtYonetmen;
+        private System.Windows.Forms.TextBox txtYil;
+        private System.Windows.Forms.TextBox txtTür;
+        private System.Windows.Forms.TextBox txtSüre;
+    }
+}
+
---
+++ vt_ornek/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace vt_ornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string connectionString = "Server=localhost;Database=film_arsiv;Uid=root;Pwd='';";
+
+        void VeriGoster()
+        {
+            using (MySqlConnection baglanti = new MySqlConnection(connectionString))
+            {
+                string sorgu = "SELECT * FROM filmler";
+
+                baglanti.Open();
+
+                MySqlCommand komut = new MySqlCommand(sorgu, baglanti);
+
+                MySqlDataAdapter adapter = new MySqlDataAdapter(komut);
+
+                DataTable tablo = new DataTable();
+
+                adapter.Fill(tablo);
+
+                dgvFilmler.DataSource = tablo;
+
+                dgvFilmler.Invalidate();
+                dgvFilmler.Refresh();
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VeriGoster();
+
+        }
+
+        private void dgvFilmler_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            txtFilmAd.Text = dgvFilmler.CurrentRow.Cells[1].Value.ToString();
+            txtYonetmen.Text = dgvFilmler.CurrentRow.Cells[2].Value.ToString();
+            txtYil.Text = dgvFilmler.CurrentRow.Cells[3].Value.ToString();
+            txtTür.Text = dgvFilmler.CurrentRow.Cells[4].Value.ToString();
+            txtSüre.Text = dgvFilmler.CurrentRow.Cells[5].Value.ToString();
+        }
+    }
+}
